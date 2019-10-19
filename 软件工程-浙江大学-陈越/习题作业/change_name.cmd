80b748a42904297079108bb7f337c4df34124e9e
Liu Awen <willowawen@gmail.com>
2019-10-19 11:19:28 +0800

    Problem sets for software engineering

---
+++ 软件工程-浙江大学-陈越/习题作业/change_name.cmd
@@ -0,0 +1,204 @@
+ren "47f79d8ac63a4172a5b150c8d5fb4714.mp4" "1-0-0软件工程概述-1.mp4"&
+ren "db53c90af7e444fc91be280ac07987f3.mp4" "2-0-0软件工程概述-2.mp4"&
+ren "ece8b5c4eb574090bd322764df5653e4.mp4" "3-0-1软件过程-1.mp4"&
+ren "2dd0ae73aa2d423a9c0349f8a6af55a8.mp4" "4-0-1软件过程-lhz.mp4"&
+ren "f32aa4145ed94a52ab3117b1a75fac18.mp4" "5-0-1软件过程-2.mp4"&
+ren "494d74703f584df5a85106d61189aff8.mp4" "6-1-0系统工程概述.mp4"&
+ren "ab2e1318564442c896ecdda44bf6533e.mp4" "7-1-1需求工程-1.mp4"&
+ren "078a010e223946ee9e8945c628bc274a.mp4" "8-1-1需求工程-2.mp4"&
+ren "985946f6826348bf9c7379d5f0d256d8.mp4" "9-1-2需求分析与模型建立-1.mp4"&
+ren "755d095884b948cb9a6aa19509bcd24a.mp4" "10-1-2需求分析与模型建立-2.mp4"&
+ren "81a698c5-9233-4ab2-83aa-7dfbceecaff4_SD.mp4" "11-2-0设计概念与原则.mp4"&
+ren "d4e29155451648a0aec3125e67650681.mp4" "12-2-1软件系统设计-1.mp4"&
+ren "5cfd17e7ade24846835314652cac6fe9.mp4" "13-2-1软件系统设计-zy.mp4"&
+ren "44ec105ffd7848a9ad4d7a45dac06d7f.mp4" "14-2-1软件系统设计-2.mp4"&
+ren "c507806781074a6099d1931a6863af8c.mp4" "15-2-2详细设计.mp4"&
+ren "ce438400ce344794a6b0a1bda6a91f8d.mp4" "16-2-3界面设计.mp4"&
+ren "f03ed2d0d2de436cb50d84beda6e80d6.mp4" "17-3-0测试策略-1.mp4"&
+ren "31754803e3b24b47a27b1f136e30739e.mp4" "18-3-0测试策略-2.mp4"&
+ren "d08e8036013b4fdf9235c28504ae0c94.mp4" "19-3-1测试方法-1.mp4"&
+ren "131d428cf0944d8e9c76c091846ecb83.mp4" "20-3-1测试方法-2.mp4"&
+ren "c8f2678cc2be4edbbec7600acfb7d311.mp4" "21-4-0Web工程概述.mp4"&
+ren "aef1a2d519654f8994b52c5ad6914b3f.mp4" "22-4-1Web项目管理.mp4"&
+ren "d9906a7fcccb437b87b6506bc15b70ad.mp4" "23-4-2Web需求分析.mp4"&
+ren "7177a3eca5b2496d83ef8775c80e8a5d.mp4" "24-4-3Web应用软件设计.mp4"&
+ren "463f23b877c94d59a4dce431cbaea45e.mp4" "25-4-4Web应用软件测试-1.mp4"&
+ren "6c140cf4a36c435eb4e3d68f4e86537b.mp4" "26-4-4Web应用软件测试-wzy.mp4"&
+ren "2b09f0ca4b9347bba38764c41c2b6dd0.mp4" "27-4-4Web应用软件测试-2.mp4"&
+ren "613fc6ac9af741a8a94f7d9b48984d09.mp4" "28-5-0软件项目管理.mp4"&
+ren "aae834b85e9c45519ad0d43f6c004a29.mp4" "29-5-1软件质量保证与更改管理.mp4"&
+ren "62ae9021ec3c463987c407794bdcf360.mp4" "30-5-2进度与风险控制-1.mp4"&
+ren "03fdece196f34669bc4502fa77ba1323.mp4" "31-5-2进度与风险控制-wzy.mp4"&
+ren "2b5b4318b1e54945884f8ed7a540d8e0.mp4" "32-5-2进度与风险控制-2.mp4"&
+ren "6c2e0115ce5c4c8094da02238735b8f9.mp4" "33-5-3软件度量.mp4"&
+ren "bd4d6df49dff4ada83daf5a7fd745fee.mp4" "34-6-0敏捷开发.mp4"&
+ren "616729_8fb92d47eddc5c00618e087ce8a5a7f9_20130911080634.ppt" "35-0-0软件工程概述.ppt"&
+ren "90b21c55-e1df-485a-b070-543c4d629838.pdf" "36-0-0软件工程概述.pdf"&
+ren "66c2d31e-a7fb-4b61-8dcf-5b05e72c553b.pdf" "37-0-0软件工程概述.pdf"&
+ren "bfbcf0b0-df02-436f-96e6-56d5b952b82e.pdf" "38-0-0软件工程概述.pdf"&
+ren "1242191_75952a48f320cf0e842b866374f3af90_20151112133157.ppt" "39-0-0The Nature of Software.ppt"&
+ren "620115_46a97fb05c8242804eb0951b1b13cab7_20130911080934.ppt" "40-0-1软件过程.ppt"&
+ren "7cfd585e-c8a9-4450-9bcb-4ef85291894e.pdf" "41-0-1软件过程.pdf"&
+ren "915547ae-4a8f-4ab3-a6ec-19380be6d7e2.pdf" "42-0-1软件过程.pdf"&
+ren "9f8f37a4-2b85-498f-8f21-135558251fcd.pdf" "43-0-1软件过程.pdf"&
+ren "1242260_11537bd43ea50e3b68e86f1910f3c4b8_20151112134455.ppt" "44-0-1Software Engineering.ppt"&
+ren "1242263_3c960e158171a1c2dc470b1bbb6ec70b_20151112134506.ppt" "45-0-1Software ProcessStructure.ppt"&
+ren "1242268_c94479f2d63aa803fbabf7046cda53e4_20151112134528.ppt" "46-0-1Process Models.ppt"&
+ren "2f427bce-a6ce-4cdb-bfed-8f9253c6749b.pdf" "47-0-2实验项目概述.pdf"&
+ren "620145_84021b7d128642ca855a74cfd4a02003_20130911081835.ppt" "48-1-0系统工程概述.ppt"&
+ren "5a6e507a-d69f-4ada-966c-c426b24812f9.pdf" "49-1-0系统工程概述.pdf"&
+ren "c505a501-cd46-4cd6-9057-1bbeae087960.pdf" "50-1-0系统工程概述.pdf"&
+ren "c6daed43-4e16-4d04-9e83-17e77d63ff6e.pdf" "51-1-0系统工程概述.pdf"&
+ren "1242285_442f0e5ee286d8ee4a88cfcaedc6f52d_20151112135114.ppt" "52-1-0Project Management Concepts.ppt"&
+ren "1242288_b04c0422ca252ab36940617975f009be_20151112135129.ppt" "53-1-0Human Aspects.ppt"&
+ren "1242298_9f6cd04cdce5af3407181dc1172d6070_20151112135212.ppt" "54-1-0Modeling Principles.ppt"&
+ren "620149_058b00e8d3c3af6cbb69b45d44a0a485_20130911082134.ppt" "55-1-1需求工程.ppt"&
+ren "c3aea7d2-f4f5-446e-a680-90b248d96e78.pdf" "56-1-1需求工程.pdf"&
+ren "673f5d05-ca82-4e89-b25c-8fea9fc08b2d.pdf" "57-1-1需求工程.pdf"&
+ren "e353bfd1-bfbc-4d05-9b35-bfc632f3ed25.pdf" "58-1-1需求工程.pdf"&
+ren "1242311_0fb0b10f2611c3479571ae4cff888bc0_20151112135239.ppt" "59-1-1Understanding Requirements.ppt"&
+ren "1242314_98c4188fe6c900cfcff5bef839cc86b4_20151112140436.ppt" "60-1-1Requirements Modeling：Scenario-Based Methods.ppt"&
+ren "620150_a61d57334313695394a66c1fe8b110dc_20130911081835.ppt" "61-1-2需求分析与模型建立.ppt"&
+ren "8cdf3895-a867-4afa-83d7-7a3a2d23091d.pdf" "62-1-2需求分析与模型建立.pdf"&
+ren "78fe73f2-d6e5-4983-b24c-f465ecf09454.pdf" "63-1-2需求分析与模型建立.pdf"&
+ren "c37504db-1ab6-46ac-96fd-d3b9a9e9eb62.pdf" "64-1-2需求分析与模型建立.pdf"&
+ren "1242352_8d718cd4a3e9b7977a000fad2375d71e_20151112141036.ppt" "65-1-2Requirements Modeling:Class-Based Methods.ppt"&
+ren "1242356_5503ec2aa2caf45a53eca926639a0891_20151112141042.ppt" "66-1-2Requirements Modeling: Behavior, Patterns, and Web Mobile Apps.ppt"&
+ren "af957f25-5112-497d-9654-77b6ef150dd4.pdf" "67-1-3需求报告要求.pdf"&
+ren "620053_b9ddcb9bb9258a85ece94070edbd649d_20130911071536.ppt" "68-2-0设计概念与原则.ppt"&
+ren "a992f6f3-95d8-4976-87b1-9746a9151c22.pdf" "69-2-0设计概念与原则.pdf"&
+ren "b9eb8be3-b351-4aa2-bc20-582685d712c1.pdf" "70-2-0设计概念与原则.pdf"&
+ren "a9da3930-e438-42c8-852f-35fe7a860eae.pdf" "71-2-0设计概念与原则.pdf"&
+ren "1253819_25083a3a3f6bc36ce971faf24cdd5f9a_20151114143455.ppt" "72-2-0Quality Concepts.ppt"&
+ren "1253823_d089b46f4bc0562882f157733c367151_20151114143513.ppt" "73-2-0Design Concepts.ppt"&
+ren "620059_c064650524da47e9963c1bcefce27a64_20130911071835.ppt" "74-2-1软件系统设计.ppt"&
+ren "0deb8545-914c-4bb8-8d05-98969887356a.pdf" "75-2-1软件系统设计.pdf"&
+ren "52e35e19-c6fc-4b99-bbd7-1d446a360c25.pdf" "76-2-1软件系统设计.pdf"&
+ren "586f0bf6-a6a8-4604-b4ef-bb56cc326620.pdf" "77-2-1软件系统设计.pdf"&
+ren "1253831_1a19e4cd94d3f93399f9f2e33def9153_20151114143529.ppt" "78-2-1Requirements Modeling:Class-Based Methods.ppt"&
+ren "620065_c6509fcc73a57baa555b6bd265567ad5_20130911072135.ppt" "79-2-2详细设计.ppt"&
+ren "e457bd14-c6db-4f65-a0a2-4646b4eb83d4.pdf" "80-2-2详细设计.pdf"&
+ren "954226a2-7153-4842-a267-2259972f4e47.pdf" "81-2-2详细设计.pdf"&
+ren "dfd711ab-1857-4a98-b808-692bbd55cda3.pdf" "82-2-2详细设计.pdf"&
+ren "1253839_251759c1a4578eba9c2ef5331041c2d1_20151114143543.ppt" "83-2-2WebApp Design.ppt"&
+ren "1253844_f90e62a794465593c476a367f5273c0f_20151114144056.ppt" "84-2-2MobileApp Design.ppt"&
+ren "1253859_0fcaf232b1830061c1a53fa7d5db428d_20151114144117.ppt" "85-2-2Review Techniques.ppt"&
+ren "1253866_d595f3b89c4f7747c30d4de2120dce34_20151114144133.ppt" "86-2-2Software Quality Assurance.ppt"&
+ren "620067_578dfae910ecd0153e04b34621ec0105_20130911072435.ppt" "87-2-3界面设计.ppt"&
+ren "a8ad179a-9e4d-4d8a-8ef7-fe986e40c290.pdf" "88-2-3界面设计.pdf"&
+ren "8a1ded44-e635-4b5e-87f2-0474c757a5b9.pdf" "89-2-3界面设计.pdf"&
+ren "72e9b10c-9548-4cb2-8773-3b5f4cda550d.pdf" "90-2-3界面设计.pdf"&
+ren "1253882_91f70f1861c3ce12f2c8be07e22feef1_20151114144243.ppt" "91-2-3Component-Level Design.ppt"&
+ren "1253894_cfac5e9d0a56e78b9d37f49c4ba84419_20151114144308.ppt" "92-2-3User Interface Design.ppt"&
+ren "1253905_bbaafa70872935d668adad85fcdd2145_20151114144347.ppt" "93-2-3Pattern-Based Design.ppt"&
+ren "3569b416-f980-477f-8ff1-b1b7c31dde07.pdf" "94-2-4设计模式研究及系统设计要求.pdf"&
+ren "620184_e0dd736bb9d3cecf80990529eeb93808_20130911083636.ppt" "95-3-0测试策略.ppt"&
+ren "6ae0c9b8-5ad7-4766-8a41-5f39d28bdd63.pdf" "96-3-0测试策略.pdf"&
+ren "46d4e91f-18d4-4994-905c-0c1c70e9fa20.pdf" "97-3-0测试策略.pdf"&
+ren "f90d30b8-8f37-4e03-8771-e33456e5aaf3.pdf" "98-3-0测试策略.pdf"&
+ren "1253917_bc3dab7a0e0b98e80ece4cda3f581e24_20151114145448.ppt" "99-3-0Software Configuration Management.ppt"&
+ren "1253934_94102b0ec01d72a6d349b3e7b6f5ada9_20151114145543.ppt" "100-3-0Software Testing Strategies.ppt"&
+ren "620190_a1a415abe7f9946664b1f0d7ca3961a4_20130911083638.ppt" "101-3-1测试方法.ppt"&
+ren "1d098c08-01e5-4dbc-bee1-2c4aacfae910.pdf" "102-3-1测试方法.pdf"&
+ren "a63a4b20-1a11-4634-b1c8-ec8cfa7c7976.pdf" "103-3-1测试方法.pdf"&
+ren "e6c66c8f-8222-424f-9223-408cd6e6f084.pdf" "104-3-1测试方法.pdf"&
+ren "1253947_10afb6c60f9a10a63c1f82c63f5feb83_20151114145612.ppt" "105-3-1Testing Conventional Applications.ppt"&
+ren "1253956_d1bc14603dc9c012313dff77921aa2db_20151114145623.ppt" "106-3-1Testing Object-Oriented Applications.ppt"&
+ren "db6d260c-4eb3-424d-9e54-ad7c657c5157.pdf" "107-3-2测试报告要求.pdf"&
+ren "620214_d00f36cd3fda0f9102f7abef858aefec_20130911084236.ppt" "108-4-0Web工程概述.ppt"&
+ren "109aff9d-28f2-48ee-b8f8-807649fef0b3.pdf" "109-4-0Web工程概述.pdf"&
+ren "f7d4d24a-cafa-4d1b-87c3-601e2b527db3.pdf" "110-4-0Web工程概述.pdf"&
+ren "1253969_7d7b96434b1a2f4529ee95fb1d330415_20151114145646.ppt" "111-4-0Testing Web Applications.ppt"&
+ren "1253976_c1025e42e1c4a62d8a247c6bff1a3063_20151114145711.ppt" "112-4-0Testing Mobile Applications.ppt"&
+ren "620221_f89982951b503e494e97fc3ad67e6d6e_20130911084236.ppt" "113-4-1Web项目管理.ppt"&
+ren "0e705195-8302-4423-a849-9c4830fbd69b.pdf" "114-4-1Web项目管理.pdf"&
+ren "cc979b37-d92a-4f4a-b52b-af37c71a1e0c.pdf" "115-4-1Web项目管理.pdf"&
+ren "1253994_ef45fd15cccd15c875200e5911265e68_20151114150304.ppt" "116-4-1Security Engineering.ppt"&
+ren "1254003_0702f8a39296bf1f32488577a8f66e69_20151114150325.ppt" "117-4-1Formal Modelingand Verification.ppt"&
+ren "1254016_5c7145a40aea897b38514f5c0088cd95_20151114150347.ppt" "118-4-1Maintenance and Reengineering.ppt"&
+ren "620224_ffcd442f3b9ff707d605b0dd487675a7_20130911084236.ppt" "119-4-2Web需求分析.ppt"&
+ren "8120f482-b953-4e11-99e0-f8b3899078ff.pdf" "120-4-2Web需求分析.pdf"&
+ren "5d5765cf-3a5d-44b1-bbdf-b76da5cac4a0.pdf" "121-4-2Web需求分析.pdf"&
+ren "0b6a08fc-ceb2-4931-b039-a4a3dea517bd.pdf" "122-4-2Web需求分析.pdf"&
+ren "1254024_3adbe02c13336aab19d33790f48f0fc2_20151114150358.ppt" "123-4-2Project Scheduling.ppt"&
+ren "1254028_f145da9eb98b2652bd0f71da443e27fb_20151114150413.ppt" "124-4-2Risk Analysis.ppt"&
+ren "1254037_689d31e279ea34dec3fde53602ea7ec0_20151114150501.ppt" "125-4-2Product Metrics.ppt"&
+ren "620228_7d58a1bd6cc15b39a5fbb3904e9a72c7_20130911084236.ppt" "126-4-3Web应用软件设计.ppt"&
+ren "9b15a897-4022-44b9-ac35-da4762f03cce.pdf" "127-4-3Web应用软件设计.pdf"&
+ren "0ba4f3a3-e100-4eab-8d38-4f6c2b757b9d.pdf" "128-4-3Web应用软件设计.pdf"&
+ren "ee6d00e2-faac-4af1-b9b6-1e42b6bb53aa.pdf" "129-4-3Web应用软件设计.pdf"&
+ren "1254050_a18b2874c88a99908eadb5d60fb8c5bb_20151114150533.ppt" "130-4-3Agile Development.ppt"&
+ren "1254068_94bbdd0b1b75da465ae3fdc759aabc08_20151114151117.ppt" "131-4-3Process and Project Metrics.ppt"&
+ren "1254076_ac9154160906ccca497cdd69d29777df_20151114151141.ppt" "132-4-3Estimation.ppt"&
+ren "620234_98ca9913746c19078995cb29e846692d_20130911084537.ppt" "133-4-4Web应用软件测试.ppt"&
+ren "5540b820-1dbd-4688-9e31-634a8557d676.pdf" "134-4-4Web应用软件测试.pdf"&
+ren "4cf41c99-2ee1-481a-9296-779e9b178f36.pdf" "135-4-4Web应用软件测试.pdf"&
+ren "0b72638e-e58b-40e7-9f6f-f00f84abcb5c.pdf" "136-4-4Web应用软件测试.pdf"&
+ren "a101f99c-9977-4f68-8e8f-40d792f8cfb8.pdf" "137-4-5WebApp研究附加题.pdf"&
+ren "620278_61b7b5eefc038506e86aa38fd10c365a_20130911085437.ppt" "138-5-0软件项目管理.ppt"&
+ren "cf8a5fbc-5460-4df6-9e78-73d80a1cac78.pdf" "139-5-0软件项目管理.pdf"&
+ren "42978ef4-2f8e-44f2-b415-7bd2d302de2c.pdf" "140-5-0软件项目管理.pdf"&
+ren "91e325d5-391a-49cf-a5ed-8e5d4a8cb0bf.pdf" "141-5-0软件项目管理.pdf"&
+ren "620284_3bddf06fd1ed04425e083df76927dc4a_20130911085436.ppt" "142-5-1软件质量保证与更改管理.ppt"&
+ren "1d67879f-c946-4983-8680-817175dc9c5d.pdf" "143-5-1软件质量保证与更改管理.pdf"&
+ren "8bb88550-ed85-431f-9e43-12a93361d0e6.pdf" "144-5-1软件质量保证与更改管理.pdf"&
+ren "25fae420-2002-4db8-ac11-3d398571b25c.pdf" "145-5-1软件质量保证与更改管理.pdf"&
+ren "620293_41d8df90861b680ff08148d942377a72_20130911085437.ppt" "146-5-2进度与风险控制.ppt"&
+ren "f399ffbc-ca7a-42bd-9a0c-0c203a0908e7.pdf" "147-5-2进度与风险控制.pdf"&
+ren "9ebda9b1-6df4-4726-af0d-03a51b9fd963.pdf" "148-5-2进度与风险控制.pdf"&
+ren "63cbbc51-b6bb-45c6-bd2b-9dc13aad8098.pdf" "149-5-2进度与风险控制.pdf"&
+ren "620307_2d1971b70ba4dcf0c0ab2ba20b1a029a_20130911085738.ppt" "150-5-3软件度量.ppt"&
+ren "d6f585be-fa25-4e63-81f5-6bc8123347a8.pdf" "151-5-3软件度量.pdf"&
+ren "d90d4197-1a2f-4666-9e9c-ec0dbe16d53d.pdf" "152-5-3软件度量.pdf"&
+ren "870f96dc-3559-452c-ba76-73357097203d.pdf" "153-5-3软件度量.pdf"&
+ren "01bccb7a-4ce0-4217-bb4e-16c8d73f264d.pdf" "154-5-4实验项目三步验收要求.pdf"&
+ren "620164_b92395f8c9a21e0f24bc5bb622613493_20130911083036.ppt" "155-6-0敏捷开发.ppt"&
+ren "af6abf07-8a1b-4ee8-a1c0-21c0f50a0333.pdf" "156-6-0敏捷开发.pdf"&
+ren "1816e08c-4411-4683-ba11-4f98aef109fa.pdf" "157-6-0敏捷开发.pdf"&
+ren "7d6d52fe-8d4d-4ad3-a749-066149603cf2.pdf" "158-6-0敏捷开发.pdf"&
+ren "5fda2f3f-58cc-42af-8102-975e08f016ae.pdf" "159-6-1前沿进展概述.pdf"&
+ren "1812ec9b-ff91-477a-be80-3b4c5281aa49.pdf" "160-其他资源-前沿进展概述.pdf"&
+ren "65b77d0f-da3f-4387-aeba-0a197cece77b.pdf" "161-其他资源-软件工程概述.pdf"&
+ren "aaef4445-b971-439e-be8b-6df5a473ca33.pdf" "162-其他资源-测试策略.pdf"&
+ren "0201be2e-439c-45c6-9e1a-697f3cfbf560.pdf" "163-习题作业-软件工程概述.pdf"&
+ren "f24093c6-50e3-44b2-a498-fff16b866938.pdf" "164-习题作业-软件过程.pdf"&
+ren "5a86cb23-848b-4687-afa1-7f497e9b2e50.pdf" "165-习题作业-系统工程概述.pdf"&
+ren "5fb943fc-097b-4d43-a71b-7b25cb981429.pdf" "166-习题作业-需求工程.pdf"&
+ren "44835d0c-c9fb-42a1-836d-5ffd2232a86b.pdf" "167-习题作业-需求分析与模型建立.pdf"&
+ren "fa5953d1-773b-4054-9abb-dde9b2ceccdf.pdf" "168-习题作业-设计概念与原则.pdf"&
+ren "a34370d8-7a20-48aa-b9b5-4df773418cf3.pdf" "169-习题作业-软件系统设计.pdf"&
+ren "749426ea-0d3f-4e24-b99b-6fca1497b65d.pdf" "170-习题作业-详细设计.pdf"&
+ren "bf94052c-2347-4e58-b6a9-59813e6fdb9a.pdf" "171-习题作业-界面设计.pdf"&
+ren "f05da7a4-4ce9-4b25-b09b-8d79df05c989.pdf" "172-习题作业-测试策略.pdf"&
+ren "56b49ebf-9c34-471b-bad3-1354fbbed3a0.pdf" "173-习题作业-测试方法.pdf"&
+ren "c714bf5d-537b-432f-86f3-f89e4064bb7b.pdf" "174-习题作业-Web工程概述.pdf"&
+ren "a3e4a67e-1e4b-47d4-90c5-d720ae1e5285.pdf" "175-习题作业-Web项目管理.pdf"&
+ren "77001856-5206-4333-9965-19b80f686513.pdf" "176-习题作业-Web需求分析.pdf"&
+ren "e3e770ae-293b-42c6-a319-b0a41227e039.pdf" "177-习题作业-Web应用软件设计.pdf"&
+ren "10accf97-de29-4520-a4e9-289aabcdae02.pdf" "178-习题作业-Web应用软件测试.pdf"&
+ren "85b834da-1397-4246-82cc-9984570841cc.pdf" "179-习题作业-软件项目管理.pdf"&
+ren "2d7f3dfb-fec3-49aa-836c-ff182da3049f.pdf" "180-习题作业-软件质量保证与更改管理.pdf"&
+ren "3dbf4bb9-db3d-415d-9b09-c46a0af7da5f.pdf" "181-习题作业-进度与风险控制.pdf"&
+ren "6bb29afe-f582-4ea0-942d-1c25117fa91e.pdf" "182-习题作业-软件度量.pdf"&
+ren "06707cb7-0385-4bdc-8ee7-acf2e1c3462b.pdf" "183-习题作业-敏捷开发.pdf"&
+ren "53458620-c4ee-4459-a70e-1aa72e2ee47b.pdf" "184-测试试卷-软件工程概述.pdf"&
+ren "27c9cede-f4c7-4b66-a987-7abbf745a832.pdf" "185-测试试卷-软件过程.pdf"&
+ren "107fd7bd-8f73-43af-b7ad-47a0085045e7.pdf" "186-测试试卷-系统工程概述.pdf"&
+ren "d1b444c4-0de0-4736-ae78-ae634a1efa4d.pdf" "187-测试试卷-需求工程.pdf"&
+ren "6a7dcb98-bc48-401c-9765-5244693a4880.pdf" "188-测试试卷-需求分析与模型建立.pdf"&
+ren "d94ecdcc-b1a4-4657-bc3b-356012640de3.pdf" "189-测试试卷-设计概念与原则.pdf"&
+ren "3e7768ef-fda4-47c2-b671-cac5cc6b4875.pdf" "190-测试试卷-软件系统设计.pdf"&
+ren "fbd0c896-c00d-4031-9e8a-4886ca5cb0a6.pdf" "191-测试试卷-详细设计.pdf"&
+ren "8b829517-5f51-4e20-b715-dddbdbaff24d.pdf" "192-测试试卷-界面设计.pdf"&
+ren "f338a0a2-91a3-454f-9717-a1e0c16d1e98.pdf" "193-测试试卷-测试策略.pdf"&
+ren "af2df6ac-f072-4368-8b38-b270fac72620.pdf" "194-测试试卷-测试方法.pdf"&
+ren "b86e4658-5684-4370-8f31-e0cd7d711c01.pdf" "195-测试试卷-Web工程概述.pdf"&
+ren "e81d886c-7469-45cb-a7a8-1fadc8b3518a.pdf" "196-测试试卷-Web项目管理.pdf"&
+ren "acc03f9d-a2f5-4eeb-840b-ed480c62049f.pdf" "197-测试试卷-Web需求分析.pdf"&
+ren "94fe6047-03d0-455c-9d81-271259bfa475.pdf" "198-测试试卷-Web应用软件设计.pdf"&
+ren "d0673a4e-8a64-41f5-afa9-333d69cc5b3d.pdf" "199-测试试卷-Web应用软件测试.pdf"&
+ren "47dd7107-7cbf-462d-a653-286b8bf7035a.pdf" "200-测试试卷-软件项目管理.pdf"&
+ren "e9884cca-bdbb-4446-83a4-7ec67a2039bd.pdf" "201-测试试卷-软件质量保证与更改管理.pdf"&
+ren "02e14a79-4215-4dcb-8577-62af456ee8bd.pdf" "202-测试试卷-进度与风险控制.pdf"&
+ren "2c963ab5-97ad-40d7-8f4f-5ff432a7a4e1.pdf" "203-测试试卷-软件度量.pdf"&
+ren "e7454629-415b-4cdd-8efe-8eef1f628931.pdf" "204-测试试卷-敏捷开发.pdf"&
